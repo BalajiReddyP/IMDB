34790f8ac48e26e694debbc11f744418e068c3e9
Balaji Reddy P <balajireddy4@gmail.com>
2021-12-27 18:38:49 +0530

    Increased page_size in pagination to 5 from 3

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination, LimitOffsetPaginatio
 
 
 class WatchListPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 5
     page_query_param = 'p'
     page_size_query_param = 'size'
     max_page_size = 10
